164c9a70291857d307c05b2022ecd9ac63a8e0f0
Magnus Hagdorn <magnus.hagdorn@charite.de>
2023-06-07 16:36:00 +0200

    refactor script as module
     * use jinja2 package directly to process templates
     * use pandas to read CSV file

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 # Certificates for The Carpentries
 
+## Using the python program
 
-There are two ways to build certificates from this repo, one depends on the python package cairosvg which in turn depends on cairo development libraries being installed. To use this method, use `bin/certificates.py` to build certificates.
+The python program makes use of the `cairosvg` package and supports reading a csv file containing participants using the `pandas` package. You can either install the python package or run it directly from this repo.
+
+```
+python3 -m learner_certificates.certificates -h
+```
+
+## Using jinja2 directly
 
 The second, pure python method uses the python packages jinja2, jinja2-cli and svglib to build the certificates.
 
 To build certificates this way, you can run:
 ```
-jinja2 swc-attendance.svg -D name="Firstname Lastname" -D date="Nov. 6, 2017" -D instructor="Some Instructor Name" > lastname_firstname.svg
+jinja2 learner_certificates/templates/swc-attendance.svg \
+   -D name="Firstname Lastname" -D date="Nov. 6, 2017" \
+   -D instructor="Some Instructor Name" > lastname_firstname.svg
 svg2pdf lastname_firstname.svg 
 ```
 
--- learner_certificates/certificates.py
+++ learner_certificates/certificates.py
@@ -10,50 +10,47 @@ Generate a certificate from a template.
 
 On a Mac, a typical command line is
 
-python bin/certificates.py \
+python -m learner_certificates.certificates \
        -b swc-instructor
-       -r $HOME/sc/certification/ \
+       -o $HOME/sc/certification/ \
        -u turing_alan
-       date='January 24, 1924' \
-       instructor='Ada Lovelace' \
-       name='Alan Turing'
+       -d 'January 24, 1924' \
+       -i 'Ada Lovelace' \
+       -n 'Alan Turing'
 
 where:
 
     -b:         BADGE_TYPE
-    -r:         ROOT_DIRECTORY
+    -o:         the OUTPUT_DIRECTORY
     -u:         USER_ID
-    name=value: fill-ins for the template
+    -d:         the date
+    -i:         the name of the instructor
+    -n:         the name of the participant
 
-The script then looks for $(ROOT_DIRECTORY)/$(BADGE_TYPE).svg as a template
-file, and creates $(ROOT_DIRECTORY)/$(BADGE_TYPE)/$(USER_ID).pdf as output.
+The script then creates $(OUTPUT_DIRECTORY)/$(BADGE_TYPE)/$(USER_ID).pdf as
+output.
 
 This script will also take a CSV file as input.  The file must contain rows of:
 
-    badge,trainer,user_id,new_instructor,email,date
+    name[,badge][,instructor][,user_id][,date]
 
 such as:
+    badge,instructor,user_id,name,date
+    swc-instructor,Grace Hopper,turing_alan,Alan Turing,2016-01-27
 
-    swc-instructor,Grace Hopper,turing_alan,Alan Turing,alan@turing.org,2016-01-27
-
-In this case, the command line invocation is:
-
-python bin/certificates.py \
-       -r $HOME/sc/certification/ \
-       -c input.csv
+Any missing columns are filled in from the command line optiomns. The order of
+the columns does not matter.
 '''
 
 import sys
-import os
-import re
-import csv
+import pandas
 import tempfile
-import subprocess
-import unicodedata
-from optparse import OptionParser
-import time
+import string
+import argparse
+from pathlib import Path
 from datetime import date
 import cairosvg
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 
 DATE_FORMAT = '%B %-d, %Y'
@@ -61,127 +58,106 @@ DATE_FORMAT = '%B %-d, %Y'
 
 def main():
     args = parse_args()
+
+    env = Environment(
+        loader=PackageLoader("learner_certificates"),
+        autoescape=select_autoescape())
+
     if args.csv_file:
-        process_csv(args)
+        check(args.csv_file.exists(), f"no such file {args.csv_file}")
+        process_csv(args, env)
     else:
-        process_single(args)
+        process_single(args, env)
+
+
+def construct_user_name(name):
+    '''construct a username from the name by only allowing
+    alphanumeric characters and replacing spaces with _'''
+    valid_characters = string.ascii_letters + string.digits + '_'
+    user_id = ''.join(
+        ch for ch in name.replace(' ', '_') if ch in valid_characters)
+    user_id = user_id.lower()
+    return user_id
 
 
 def parse_args():
     '''Get command-line arguments.'''
 
-    parser = OptionParser()
-    # -i argument retained for backward incompatibility,
-    #  not used anymore
-    parser.add_option('-i', '--ink',
-                      default='/Applications/Inkscape.app/Contents/Resources/bin/inkscape',
-                      dest='inkscape',
-                      help='[deprecated] Path to Inkscape')
-    parser.add_option('-b', '--badge',
-                      default=None, dest='badge_type', help='Type of badge')
-    parser.add_option('-r', '--root',
-                      default=os.getcwd(), dest='root_dir', help='Root directory (current by default)')
-    parser.add_option('-u', '--userid',
-                      default=None, dest='user_id', help='User ID')
-    parser.add_option('-c', '--csv',
-                      default=None, dest='csv_file', help='CSV file')
-
-    args, extras = parser.parse_args()
-    check(args.root_dir is not None, 'Must specify root directory')
-    msg = 'Must specify either CSV file or both root directory and user ID'
-    if args.csv_file is not None:
-        check((args.badge_type is None) and (args.user_id is None), msg)
-    elif args.badge_type and args.user_id:
-        check(args.csv_file is None, msg)
-    else:
-        check(False, msg)
-
-    args.params = extract_parameters(extras)
-    if 'date' not in args.params:
-        args.params['date'] = date.strftime(date.today(), DATE_FORMAT)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-b', '--badge', dest='badge_type', help='Type of badge')
+    parser.add_argument(
+        '-o', '--output-dir', default=Path.cwd(), type=Path,
+        help='Output directory (current by default)')
+    parser.add_argument(
+        '-d', '--date', default=date.isoformat(date.today()),
+        help='Date of certificate, defaults to today')
+    parser.add_argument(
+        '-i', '--instructor', help='Name of instructor')
+    grp = parser.add_mutually_exclusive_group(required=True)
+    grp.add_argument(
+        '-c', '--csv', type=Path, dest='csv_file', help='CSV file')
+    grp.add_argument(
+        '-n', '--name', help='Name of the participant')
+    parser.add_argument(
+        '-u', '--userid', dest='user_id',
+        help='User ID, default construct from name')
+    args = parser.parse_args()
 
     return args
 
 
-def extract_parameters(args):
-    '''Extract key-value pairs from command line (checking for uniqueness).'''
-
-    result = {}
-    for a in args:
-        fields = a.split('=')
-        assert len(fields) == 2, 'Badly formatted key-value pair "{0}"'.format(a)
-        key, value = fields
-        assert key not in result, 'Duplicate key "{0}"'.format(key)
-        result[key] = value
-    return result
-
-
-def process_csv(args):
+def process_csv(args, env):
     '''Process a CSV file.'''
 
-    with open(args.csv_file, 'r') as raw:
-        reader = csv.reader(raw)
-        for row in reader:
-            check(len(row) == 6, 'Badly-formatted row in CSV: {0}'.format(row))
-            badge_type, args.params['instructor'], user_id, args.params['name'], email, args.params['date'] = row
-            if '-' in args.params['date']:
-                d = time.strptime(args.params['date'], '%Y-%m-%d')
-                d = date(*d[:3])
-                args.params['date'] = date.strftime(d, DATE_FORMAT)
-            template_path = construct_template_path(args.root_dir, badge_type)
-            output_path = construct_output_path(args.root_dir, badge_type, user_id)
-            create_certificate(template_path, output_path, args.params)
+    data = pandas.read_csv(args.csv_file)
+    if 'instructor' not in data.columns:
+        check(args.instructor is not None, "need to specify instructor")
+        data['instructor'] = args.instructor
+    if 'date' not in data.columns:
+        data['date'] = args.date
+    if 'user_id' not in data.columns:
+        data['user_id'] = data['name'].apply(construct_user_name)
+    if 'badge' not in data.columns:
+        check(args.badge_type is not None, "need to specify badge type")
+        data['badge'] = args.badge_type
+    for _, row in data.iterrows():
+        create_certificate(
+            row['badge'], row['instructor'], row['user_id'], row['name'],
+            row['date'], args.output_dir, env)
 
-def process_single(args):
+
+def process_single(args, env):
     '''Process a single entry.'''
 
-    template_path = construct_template_path(args.root_dir, args.badge_type)
-    output_path = construct_output_path(args.root_dir, args.badge_type, args.user_id)
-    create_certificate(template_path, output_path, args.params)
+    check(args.instructor is not None, "need to specify instructor")
+    check(args.badge_type is not None, "need to specify badge type")
+
+    if args.user_id is None:
+        user_id = construct_user_name(args.name)
+    else:
+        user_id = args.user_id
+
+    create_certificate(args.badge_type, args.instructor, user_id,
+                       args.name, args.date, args.output_dir, env)
 
 
-def construct_template_path(root_dir, badge_type):
-    '''Create path for template file.'''
-
-    return os.path.join(root_dir, badge_type + '.svg')
-
-
-def construct_output_path(root_dir, badge_type, user_id):
-    '''Create path for generated PDF certificate.'''
-
-    badge_path = os.path.join(root_dir, badge_type)
-    path_exists = os.path.exists(badge_path)
-
-    if not path_exists:
-        os.mkdir(badge_path)
-
-    return os.path.join(badge_path, user_id + '.pdf')
-
-
-def create_certificate(template_path, output_path, params):
+def create_certificate(
+        badge_type, instructor, user_id, name, datestr, output, env):
     '''Create a single certificate.'''
 
-    with open(template_path, 'r') as reader:
-        template = reader.read()
-    check_template(template, params)
+    template = env.get_template(badge_type + ".svg")
+    badge_path = output / Path(badge_type)
 
-    for key, value in params.items():
-        pattern = '{{' + key + '}}'
-        template = template.replace(pattern, value)
+    if not badge_path.exists():
+        badge_path.mkdir()
+    outputpdf = badge_path / Path(user_id).with_suffix('.pdf')
 
     tmp = tempfile.NamedTemporaryFile(suffix='.svg', delete=False)
-    tmp.write(bytes(template, 'utf-8'))
-
-    cairosvg.svg2pdf(url=tmp.name, write_to=output_path, dpi=90)
-
-
-def check_template(template, params):
-    '''Check that all values required by template are present.'''
-
-    expected = re.findall(r'\{\{([^}]*)\}\}', template)
-    missing = set(expected) - set(params.keys())
-    check(not missing,
-          'Missing parameters required by template: {0}'.format(' '.join(missing)))
+    tmp.write(bytes(template.render(
+        name=name, instructor=instructor,
+        date=date.fromisoformat(datestr).strftime(DATE_FORMAT)), 'utf-8'))
+    cairosvg.svg2pdf(url=tmp.name, write_to=str(outputpdf), dpi=90)
 
 
 def check(condition, message):
